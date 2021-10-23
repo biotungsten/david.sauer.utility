2db9f2c6193a537c06011cd5acc5894e07a345ad
David Sauer <sauerdavid13@icloud.com>
2021-10-23 18:52:02 +0200

    fix for control entries

--- R/appl2.R
+++ R/appl2.R
@@ -1,8 +1,8 @@
-#' Converts a label of the format X...-Y-Z where X is the substance abbreviation and Y is the concentration and Z is a number indicating the batch. CTRL is treated as abbreviation for control.
+#' Converts a label of the format X...-Y-Z where X is the substance abbreviation and Y is the concentration and Z is a number indicating the batch. CTRL is treated as abbreviation for control. CTRL must also contain any number as concentration and its respective batch entry.
 #' @export
 appl2.label.as.vector <- function(label){
   if(str_starts(label, "CTRL")){
-    return(c("control", 0))
+    return(c("control", 0, as.numeric(str_split(label, "-")[[1]][3])))
   }
   substance.abbreviation <- str_sub(label, 1, 1)
   substance.name <- switch(substance.abbreviation,
