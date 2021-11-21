bc6322087b1e95cf321f1cb36a598b58341f13e1
David Sauer <sauerdavid13@icloud.com>
2021-11-21 16:05:20 +0100

    Usage of alternate implementation of as.numeric.factor

--- R/utility.R
+++ R/utility.R
@@ -1,17 +1,9 @@
 #' TBD
-#' @export
-my.as.numeric.factor <- function(x) {
-  return(as.numeric(as.character(x)))
+as.numeric.factor <- function(x) {
+  return(as.numeric(as.character(x[,])))
 }
 
 #' TBD
-#' @export
 as.idx.colname <- function(data, name) {
   return(grep(paste("^",name,"$", sep=""), colnames(data)))
-}
-
-#' TBD
-#' @export
-as.idx.rowname <- function(data, name) {
-  return(grep(paste("^",name,"$", sep=""), rownames(data)))
 }
