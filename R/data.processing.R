5b4e7353010998705617bf6faee96839d542abc7
David Sauer <sauerdavid13@icloud.com>
2021-11-21 18:51:37 +0100

    quick fixes and added capping func

--- R/data.processing.R
+++ R/data.processing.R
@@ -12,3 +12,24 @@ filter.data.by.substances <- function(data, substance.names, substance.colname =
   }
   return(filtered.data)
 }
+
+#' Cleans a dataframe of normalized data from values bigger than 1.
+#' @param mode What to do with data bigger 1 (1 - is capped at 1, 2- is set to NA, default - do nothing)
+#' @note It is persumed, that control data is marked by teh entry 'control' in a column named 'substance'
+#' @export
+cap.normalized.data <- function(data, mode, normal.colname = "response"){
+  data <- apply(data, 1, function(x) {
+    if (as.numeric(x[as.idx.colname(data, normal.colname)]) > 1 &
+        !is.na(x[as.idx.colname(data, normal.colname)]) &
+        x[as.idx.colname(data, "substance")] != "control") {
+      if(mode == 1) {
+        x[as.idx.colname(data, normal.colname)] <- 1
+      }
+      if (mode == 2) {
+        x[as.idx.colname(data, normal.colname)] <- NA
+      }
+    }
+    return(x)
+  })
+  return(data.frame(t(data))) #we have to transpose the return due to the workings of lapply
+}
